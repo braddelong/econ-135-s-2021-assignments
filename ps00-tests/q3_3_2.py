bc227ee7c08ddc28b4e4bb74a0c23390177b5c6a
James Bradford DeLong <delong@econ.berkeley.edu>
2021-01-17 14:18:43 -0800

    econ 135 assignments

---
+++ ps00-tests/q3_3_2.py
@@ -0,0 +1,22 @@
+test = {
+  'name': '3_3_2',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> round(min_height_difference, 5)
+          0.04
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
